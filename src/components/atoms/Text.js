8c75b0978f6b35b81241c30beb924f64ff412ad1
brjatoba92 <brjatoba92@gmail.com>
2023-12-02 16:37:02 -0300

    feat - ChakraUI and Atomic Design Implementation -  Components - Text

---
+++ src/components/atoms/Text.js
@@ -0,0 +1,18 @@
+import { Text as ChakraText } from '@chakra-ui/react'
+
+export const Text = ({ children, ...props }) => (
+  <ChakraText {...props}>{children}</ChakraText>
+)
+
+Text.ScreenTitle = ({ children, ...props }) => (
+  <ChakraText
+    fontSize='20px'
+    fontWeight='800'
+    color='brand.black'
+    {...props}
+  >
+    {children}
+  </ChakraText>
+)
+
+Text.ScreenTitle.displayName = 'ScreenTitle'
