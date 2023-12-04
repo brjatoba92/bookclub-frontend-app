825f4dcd374d1e3e49d397ff2eb36404b9095217
brjatoba92 <brjatoba92@gmail.com>
2023-12-04 12:13:22 -0300

    feat - ChakraUI and Atomic Design Implementation -  Responsiveness

--- src/modules/unauthenticated/screen/Login/index.js
+++ src/modules/unauthenticated/screen/Login/index.js
@@ -4,30 +4,33 @@ import { Text, Input, Link, Button } from 'components'
 export const LoginScreen = () => {
   return (
         <Flex flexDir={'row'} w='100vw' h='100vh'>
-            <Flex alignItems='flex-start' justifyContent='center' paddingLeft='112px' flexDir={'column'} w={'40%'} h={'100%'}>
-                <Image
-                    h={'48px'}
-                    w={'160px'}
-                    src='/img/logo.svg'
-                    alt='BookClub Logo'
-                />
-                <Flex flexDir='column' w='416px'>
-                    <Text.ScreenTitle mt='48px'>Login</Text.ScreenTitle>
-                    <Input mt='24px' placeholder='user@email.com' />
-                    <Input.Password mt='16px' placeholder='*****'/>
-                    <Flex mt='8px' w='100%' alignItems='flex-end' justifyContent='flex-end'>
-                      <Link>Forgot Password</Link>
-                    </Flex>
-                    <Button mt='24px'>Login</Button>
-                    <Link.Action
-                      mt='48px'
-                      text='No have account?'
-                      actionText='Sigup Now'
-                    />
+            <Flex
+              alignItems={['center', 'flex-start']}
+              justifyContent='center'
+              paddingLeft={['0px', '0px', '0px', '112px']}
+              padding={['24px', '48px', '80px', '0px']}
+              flexDir={'column'}
+              w={['100%', '100%', '100%', '40%']}
+              h={'100%'}
+            >
+              <Flex flexDir='column' w={['100%', '100%', '100%', '416px']}>
+                <Image h={'48px'} w={'160px'} src='/img/logo.svg' alt='BookClub Logo' />
+                <Text.ScreenTitle mt='48px'>Login</Text.ScreenTitle>
+                <Input mt='24px' placeholder='user@email.com' />
+                <Input.Password mt='16px' placeholder='*****'/>
+                <Flex mt='8px' w='100%' alignItems='flex-end' justifyContent='flex-end'>
+                  <Link>Forgot Password</Link>
                 </Flex>
+                <Button mt='24px'>Login</Button>
+                  <Link.Action
+                    mt='48px'
+                    text='No have account?'
+                    actionText='Sigup Now'
+                  />
+              </Flex>
             </Flex>
             <Flex
-              w={'60%'}
+              w={['0%', '0%', '0%', '60%']}
               h={'100vh'}
               bgImage="url('/img/auth-background.svg')"
               bgSize="cover"
