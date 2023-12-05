1b0fb41ad955f5b35b9275f3bcb4662389dc0fce
brjatoba92 <brjatoba92@gmail.com>
2023-12-05 20:03:38 -0300

    feat - React Router Dom -  First commit

---
+++ src/modules/unauthenticated/routes/index.js
@@ -0,0 +1,8 @@
+import { LoginScreen } from '../screen'
+
+export const unauthRoutes = [
+  {
+    path: '/',
+    element: <LoginScreen />
+  }
+]
--- src/modules/unauthenticated/screen/Login/index.js
+++ src/modules/unauthenticated/screen/Login/index.js
@@ -6,19 +6,23 @@ export const LoginScreen = () => {
         <Flex flexDir={'row'} w='100vw' h='100vh'>
             <Flex
               alignItems={['center', 'flex-start']}
-              justifyContent='center'
-              paddingLeft={['0px', '0px', '0px', '112px']}
-              padding={['24px', '48px', '80px', '0px']}
-              flexDir={'column'}
+              justifyContent={['center']}
+              padding={['24px', '48px', '80px', '112px']}
+              flexDir='column'
               w={['100%', '100%', '100%', '40%']}
-              h={'100%'}
+              h='100%'
             >
               <Flex flexDir='column' w={['100%', '100%', '100%', '416px']}>
                 <Image h={'48px'} w={'160px'} src='/img/logo.svg' alt='BookClub Logo' />
                 <Text.ScreenTitle mt='48px'>Login</Text.ScreenTitle>
                 <Input mt='24px' placeholder='user@email.com' />
                 <Input.Password mt='16px' placeholder='*****'/>
-                <Flex mt='8px' w='100%' alignItems='flex-end' justifyContent='flex-end'>
+                <Flex
+                  mt='8px'
+                  w='100%'
+                  alignItems='flex-end'
+                  justifyContent='flex-end'
+                >
                   <Link>Forgot Password</Link>
                 </Flex>
                 <Button mt='24px'>Login</Button>
