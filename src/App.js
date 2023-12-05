89ff6e96c11d5e6e0200618064df48f41bf7eff0
brjatoba92 <brjatoba92@gmail.com>
2023-12-05 20:03:38 -0300

    feat - React Router Dom -  First commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from 'styles'
-import { LoginScreen } from 'modules/unauthenticated/screen'
+// import { LoginScreen } from 'modules/unauthenticated/screen'
+import { RouterProvider } from 'react-router-dom'
+import { router } from 'router'
 
 function App () {
   return (
     <ChakraProvider theme={theme}>
-      <LoginScreen></LoginScreen>
+      <RouterProvider router={router} />
     </ChakraProvider>
   )
 }
