79760adda07da8147898481a470dbcc91e31fd15
brjatoba92 <brjatoba92@gmail.com>
2023-12-06 21:01:21 -0300

    feat - Forms implementation with Chakra UI Formik and Yup - First Commit

--- src/modules/unauthenticated/screen/Login/index.js
+++ src/modules/unauthenticated/screen/Login/index.js
@@ -1,9 +1,26 @@
 import { Flex, Image } from '@chakra-ui/react'
 import { Text, Input, Link, Button } from 'components'
 import { useNavigate } from 'react-router-dom'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
 
 export const LoginScreen = () => {
   const navigate = useNavigate()
+  const { handleSubmit, values, handleChange, errors } = useFormik({
+    initialValues: {
+      email: '',
+      password: ''
+    },
+    validationSchema: Yup.object({
+      email: Yup.string().email('E-mail invalido').required('E-mail é obrigatorio'),
+      password: Yup.string().min(6, 'senha deve ter ao menos 6 caracteres').required('Senha é obrigatorio')
+    }),
+    onSubmit: (data) => {
+      console.log({ data })
+    }
+  })
+  console.log({ values, errors })
+
   return (
         <Flex flexDir={'row'} w='100vw' h='100vh'>
             <Flex
@@ -17,8 +34,22 @@ export const LoginScreen = () => {
               <Flex flexDir='column' w={['100%', '100%', '100%', '416px']}>
                 <Image h={'48px'} w={'160px'} src='/img/logo.svg' alt='BookClub Logo' />
                 <Text.ScreenTitle mt='48px'>Login</Text.ScreenTitle>
-                <Input mt='24px' placeholder='user@email.com' />
-                <Input.Password mt='16px' placeholder='*****'/>
+                <Input
+                  id='email'
+                  name='email'
+                  value={values.email}
+                  mt='24px'
+                  placeholder='user@email.com'
+                  onChange={handleChange}
+                />
+                <Input.Password
+                  id='password'
+                  name='password'
+                  value={values.password}
+                  mt='16px'
+                  placeholder='*****'
+                  onChange={handleChange}
+                />
                 <Flex
                   mt='8px'
                   w='100%'
@@ -29,7 +60,7 @@ export const LoginScreen = () => {
                     Forgot Password
                   </Link>
                 </Flex>
-                <Button mb='12px' mt='24px'>Login</Button>
+                <Button onClick={handleSubmit} mb='12px' mt='24px'>Login</Button>
                   <Link.Action
                     onClick={() => navigate('/sigup')}
                     mt='8px'
