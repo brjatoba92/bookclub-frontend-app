f20fa1ce98b10c42a7f66e7a5e10d9dd883833a5
brjatoba92 <brjatoba92@gmail.com>
2024-01-18 17:33:21 -0300

    feat - Navbar implementation - First Commit - NavBar, SearchBar and UserMenu components

--- src/modules/authenticated/screen/Home/index.js
+++ src/modules/authenticated/screen/Home/index.js
@@ -1,10 +1,13 @@
 import { Flex } from '@chakra-ui/react'
 import { useSelector } from 'react-redux'
+import { NavBar } from 'components'
 
 export const HomeScreen = () => {
   const userStore = useSelector((state) => state.user)
   console.log({ userStore })
   return (
-      <Flex />
+      <Flex>
+        <NavBar/>
+      </Flex>
   )
 }
