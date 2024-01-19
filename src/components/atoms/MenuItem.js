561d8b950aa059c950b61b95b82e28e9a2378a41
brjatoba92 <brjatoba92@gmail.com>
2024-01-19 19:36:12 -0300

    User Menu implementation - Menu Item Created - React Icons use

---
+++ src/components/atoms/MenuItem.js
@@ -0,0 +1,9 @@
+import { MenuItem as ChakraMenuItem, Icon } from '@chakra-ui/react'
+import { Text } from './Text'
+
+export const MenuItem = ({ icon, text, divider }) => (
+    <ChakraMenuItem h='48px'>
+      <Icon as={icon} mr='8px' boxSize='18px' color='brand.greyDark' borderBottomWidth={ divider ? '1px' : '0px' } borderBottomStyle='solid' borderBottomColor='brand.grayLight'/>
+      <Text color='brand.greyDark' size='14px' fontWeight='600'>{text}</Text>
+    </ChakraMenuItem>
+)
