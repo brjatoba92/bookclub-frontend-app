8dd50cb6a6eb0206a2fe15aecd6291336c4ea1b9
brjatoba92 <brjatoba92@gmail.com>
2024-01-19 19:36:12 -0300

    User Menu implementation - Menu Item Created - React Icons use

--- src/components/molecules/UserMenu.js
+++ src/components/molecules/UserMenu.js
@@ -1,10 +1,52 @@
-import { Avatar, Menu, MenuButton, MenuItem, MenuList, Flex } from '@chakra-ui/react'
+import { Avatar, Menu, MenuButton, MenuList, Flex } from '@chakra-ui/react'
 import { useSelector } from 'react-redux'
-import { Text } from 'components/atoms'
+import { Text, MenuItem } from 'components/atoms'
+import { BsBookmarkHeart, BsPersonCheck } from 'react-icons/bs'
+import { RiLockPasswordLine } from 'react-icons/ri'
+import { IoDocumentTextOutline, IoDocumentOutline } from 'react-icons/io5'
+import { MdLogout } from 'react-icons/md'
 
 export const UserMenu = () => {
   const userStore = useSelector((state) => state.user)
   console.log({ userStore })
+  const menuOptions = [
+    {
+      id: 0,
+      icon: BsBookmarkHeart,
+      text: 'Favorites',
+      divider: false
+    },
+    {
+      id: 1,
+      icon: BsPersonCheck,
+      text: 'User data',
+      divider: false
+    },
+    {
+      id: 2,
+      icon: RiLockPasswordLine,
+      text: 'Change password',
+      divider: true
+    },
+    {
+      id: 3,
+      icon: IoDocumentTextOutline,
+      text: 'Terms of use',
+      divider: false
+    },
+    {
+      id: 4,
+      icon: IoDocumentOutline,
+      text: 'Privacy Policy',
+      divider: true
+    },
+    {
+      id: 5,
+      icon: MdLogout,
+      text: 'Logout',
+      divider: false
+    }
+  ]
   return (
     <Menu>
       <MenuButton>
@@ -24,9 +66,17 @@ export const UserMenu = () => {
         </Flex>
       </MenuButton>
       <MenuList>
-        <MenuItem>Favorites</MenuItem>
-        <MenuItem>Data User</MenuItem>
-        <MenuItem></MenuItem>
+        {
+          menuOptions.map((item) =>
+            <MenuItem
+              key={`menu_item_${item.id}`}
+              id={item.id}
+              icon={item.icon}
+              text={item.text}
+              divider={item.divider}
+            />
+          )
+        }
       </MenuList>
     </Menu>
   )
