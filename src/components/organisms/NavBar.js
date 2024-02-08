92e393cc159f9cd6b91fc291157c64badbfcbde4
brjatoba92 <brjatoba92@gmail.com>
2024-02-08 11:26:51 -0300

    feat: User Modal implementation

--- src/components/organisms/NavBar.js
+++ src/components/organisms/NavBar.js
@@ -1,31 +1,39 @@
+import { useState } from 'react'
 import { Flex, Image } from '@chakra-ui/react'
 import { SearchBar, UserMenu } from 'components/molecules'
 import { useNavigate } from 'react-router-dom'
+import { UserModal } from './UserModal'
 
 export const NavBar = ({ query, setQuery }) => {
+  const [showModal, setShowModal] = useState()
   const navigate = useNavigate()
+  const onCloseModal = () => {
+    setShowModal(null)
+  }
   return (
     <Flex
-      w='100vw'
-      flexDir='row'
-      alignItems='center'
-      justifyContent='space-between'
+      w="100vw"
+      flexDir="row"
+      alignItems="center"
+      justifyContent="space-between"
       paddingX={['24px', '48px', '80px', '112px']}
-      paddingTop='24px'
-
+      paddingTop="24px"
     >
       <Image
         h={'48px'}
         w={['100px', '160px']}
-        src='/img/logo.svg'
-        alt='BookClub Logo'
-        cursor='pointer'
+        src="/img/logo.svg"
+        alt="BookClub Logo"
+        cursor="pointer"
         onClick={() => navigate('/home')}
       />
       <Flex display={['none', 'flex']}>
-        <SearchBar query={query} setQuery={setQuery}/>
+        <SearchBar query={query} setQuery={setQuery} />
       </Flex>
-      <UserMenu/>
+
+      <UserMenu setShowModal={setShowModal} />
+
+      {showModal === 'user' && <UserModal onClose={onCloseModal} />}
     </Flex>
   )
 }
