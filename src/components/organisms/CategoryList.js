12a8ad2b5fbd34d9a588c597ea333084ac20879f
brjatoba92 <brjatoba92@gmail.com>
2024-02-12 21:54:32 -0300

    fix: query categories error cache

--- src/components/organisms/CategoryList.js
+++ src/components/organisms/CategoryList.js
@@ -7,16 +7,19 @@ import { getBooksByCategory, getCategories } from 'services/api/requests'
 
 export const CategoryList = ({ title, categoryId }) => {
   const [selected, setSelected] = useState(categoryId)
+
   const { data } = useQuery('categories', getCategories)
   const {
     data: bookQuery,
     refetch,
     isLoading
   } = useQuery(
-    ['booksById', selected],
+    [`booksById-${selected}`, selected],
     () => getBooksByCategory(selected),
     {
-      enabled: !!selected
+      enabled: !!selected,
+      refetchOnWindowFocus: true,
+      refetchOnMount: true
     }
   )
 
@@ -27,7 +30,10 @@ export const CategoryList = ({ title, categoryId }) => {
   }, [data])
 
   useEffect(() => {
-    refetch()
+    if (categoryId) {
+      setSelected(categoryId)
+      refetch()
+    }
   }, [categoryId])
 
   return (
@@ -36,7 +42,7 @@ export const CategoryList = ({ title, categoryId }) => {
       mt={['24px', '48px']}
       paddingX={['24px', '48px', '80px', '112px']}
       overflowX={['scroll', 'auto']}
-      h='520px'
+      h="520px"
       css={{
         '::-webkit-scrollbar': {
           display: 'none'
@@ -44,35 +50,33 @@ export const CategoryList = ({ title, categoryId }) => {
       }}
     >
       <Text.ScreenTitle>{title || 'Categories'}</Text.ScreenTitle>
-      {
-        !categoryId && (
-          <Flex
-            mt='12px'
-            flexDir='row'
-            overflowX={['scroll', 'auto']}
-            css={{
-              '::-webkit-scrollbar': {
-                display: 'none'
-              }
-            }}
-          >
-        {data?.data &&
-          data?.data.map((item) =>
-            <CategoryCard
-              key={`book_${item.id}`}
-              selected={selected === item.id}
-              onClick={() => setSelected(item.id)}
-              {...item}
-          />
-          )}
+      {!categoryId && (
+        <Flex
+          mt="12px"
+          flexDir="row"
+          overflowX={['scroll', 'auto']}
+          css={{
+            '::-webkit-scrollbar': {
+              display: 'none'
+            }
+          }}
+        >
+          {data?.data &&
+            data?.data.map((item) => (
+              <CategoryCard
+                key={`book_${item.id}`}
+                selected={selected === item.id}
+                onClick={() => setSelected(item.id)}
+                {...item}
+              />
+            ))}
         </Flex>
-        )
-      }
+      )}
       <Flex
-        mt='12px'
+        mt="12px"
         flexDir="row"
-        pb='48px'
-        cursor='pointer'
+        pb="48px"
+        cursor="pointer"
         overflowX={['scroll', 'auto']}
         css={{
           '::-webkit-scrollbar': {
@@ -80,18 +84,13 @@ export const CategoryList = ({ title, categoryId }) => {
           }
         }}
       >
-        {
-          isLoading && (
-            <Loader />
-          )}
-        {
-          (!isLoading && bookQuery?.data?.lenght === 0) && (
-            <EmptyMessage>Nenhum livro encontrado</EmptyMessage>
-          )
-        }
+        {isLoading && <Loader />}
+        {!isLoading && bookQuery && bookQuery?.data?.lenght === 0 && (
+          <EmptyMessage>Nenhum livro encontrado</EmptyMessage>
+        )}
         {bookQuery &&
           bookQuery?.data.map((item) => (
-          <BookCard key={`book_${item.id}`} {...item} />
+            <BookCard key={`book_${item.id}`} {...item} />
           ))}
       </Flex>
     </Flex>
